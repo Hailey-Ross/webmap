89928a33481ff5715a5e26bf884aa899cd7dbeaf
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2023-03-24 23:16:00 -0600

    Config Cleanup

--- config.lua
+++ config.lua
@@ -1,13 +1,14 @@
-Config = {}
+Config = {
 
 -- How often the server queries information from clients
-Config.updateInterval = 5000
+updateInterval = 5000
 
 -- Enable or disable weather info on the live map (requires weathersync if enabled)
-Config.displayWeather = false
+displayWeather = false
 
 -- Enable or disable Logging of user logins (debug)
-Config.loggingToggle = false
+loggingToggle = false
 
 -- Enable or disable Password Protection
-Config.passwordProtection = true  --true/false only
+passwordProtection = true  --true/false only
+}
