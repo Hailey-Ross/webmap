5ef5c08bef0f0fcbdfb74ee9554b3e8646448f30
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2023-03-26 03:06:27 -0600

    Version Control Patch, Minor changes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,13 +3,13 @@ games {"gta5", "rdr3"}
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
 name "webmap"
-author "kibukj"
+author "kibukj | Fork by Hailey"
 description "Live map for FiveM and RedM servers"
-url "https://github.com/kibook/webmap"
+url "https://github.com/Hailey-Ross/webmap"
 
 dependency "httpmanager" -- https://github.com/kibook/httpmanager
 
--- You can comment this out if you turn off Config.displayWeather
+-- You can comment this out if you turn off Display Weather in Config
 dependency "weathersync" -- https://github.com/kibook/weathersync
 
 server_scripts {
@@ -18,3 +18,8 @@ server_scripts {
 }
 
 client_script "client.lua"
+
+version '0.1'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/Hailey-Ross/webmap'
