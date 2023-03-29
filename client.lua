a210b4a3acfec84df542afed4e327920c9430092
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2023-03-28 18:57:05 -0600

    Player ID Test Fix

--- client.lua
+++ client.lua
@@ -2,10 +2,11 @@ RegisterNetEvent("webmap:updateInfo")
 
 AddEventHandler("webmap:updateInfo", function()
 	local playerPed = PlayerPedId()
+	local playerID = PlayerId()
 
 	TriggerServerEvent("webmap:updateInfo", {
-		name = GetPlayerName(PlayerId()),
-		pid = GetPlayerServerId(PlayerId()),
+		name = GetPlayerName(playerID),
+		pid = GetPlayerServerId(playerID),
 		coords = GetEntityCoords(playerPed),
 		heading = GetEntityHeading(playerPed),
 		health = GetEntityHealth(playerPed)
